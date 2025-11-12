099b7fe5ff7d157b327f1cec0e775954589cee2b
Matthias-Yan <matthias.yan.0609@gmail.com>
2025-11-12 12:31:21 +0000

    Include actuator configurations

--- source/Walk/Walk/tasks/manager_based/walk/GR1_T1.py
+++ source/Walk/Walk/tasks/manager_based/walk/GR1_T1.py
@@ -22,4 +22,89 @@ GR1_T1_CFG = ArticulationCfg(
         joint_pos = {},
         joint_vel = {},
     ),
+    actuators = {
+    "torso": ImplicitActuatorCfg(
+        joint_names=[
+            "waist_yaw",
+            "waist_pitch",
+            "waist_roll",
+        ],
+        effort_limit_sim=120.0,
+        velocity_limit_sim=100.0,
+        stiffness=0.0,
+        damping=0.0,
+    ),
+
+    "left_leg": ImplicitActuatorCfg(
+        joint_names=[
+            "l_hip_roll",
+            "l_hip_yaw",
+            "l_knee_pitch",
+            "l_ankle_pitch",
+            "l_ankle_roll",
+        ],
+        effort_limit_sim=300.0,
+        velocity_limit_sim=100.0,
+        stiffness=0.0,
+        damping=0.0,
+    ),
+
+    "right_leg": ImplicitActuatorCfg(
+        joint_names=[
+            "r_hip_roll",
+            "r_hip_yaw",
+            "r_hip_pitch",
+            "r_knee_pitch",
+            "r_ankle_pitch",
+            "r_ankle_roll",
+        ],
+        effort_limit_sim=300.0,
+        velocity_limit_sim=100.0,
+        stiffness=0.0,
+        damping=0.0,
+    ),
+
+    "head": ImplicitActuatorCfg(
+        joint_names=[
+            "head_yaw",
+            "head_roll",
+            "head_pitch",
+        ],
+        effort_limit_sim=80.0,
+        velocity_limit_sim=100.0,
+        stiffness=0.0,
+        damping=0.0,
+    ),
+
+    "left_arm": ImplicitActuatorCfg(
+        joint_names=[
+            "l_shoulder_roll",
+            "l_shoulder_yaw",
+            "l_elbow_pitch",
+            "l_wrist_yaw",
+            "l_wrist_roll",
+            "l_wrist_pitch",
+        ],
+        effort_limit_sim=120.0,
+        velocity_limit_sim=100.0,
+        stiffness=0.0,
+        damping=0.0,
+    ),
+
+    "right_arm": ImplicitActuatorCfg(
+        joint_names=[
+            "r_shoulder_pitch",
+            "r_shoulder_roll",
+            "r_shoulder_yaw",
+            "r_elbow_pitch",
+            "r_wrist_yaw",
+            "r_wrist_roll",
+            "r_wrist_pitch",
+        ],
+        effort_limit_sim=120.0,
+        velocity_limit_sim=100.0,
+        stiffness=0.0,
+        damping=0.0,
+    ),
+}
 )
