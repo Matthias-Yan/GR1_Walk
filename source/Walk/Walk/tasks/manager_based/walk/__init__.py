0ba09f9d32e57a225a20b39bb89c970c5167e9fb
Matthias-Yan <matthias.yan.0609@gmail.com>
2025-11-13 02:46:51 +0000

    Included gym register for Play

--- source/Walk/Walk/tasks/manager_based/walk/__init__.py
+++ source/Walk/Walk/tasks/manager_based/walk/__init__.py
@@ -21,3 +21,13 @@ gym.register(
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
 )
+
+gym.register(
+    id="Template-Walk-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.walk_env_cfg:WalkEnvCfg_PLAY",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
